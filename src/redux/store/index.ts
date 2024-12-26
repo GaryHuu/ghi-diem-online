36626b1a43a9399eb702eece59f2de3cc1f2f76f
Tan IT <tandev151>
2024-12-26 22:39:50 +0700

    handle joyride guide

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,11 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import guideReducer from '@/redux/slices/guideSlice';
 import matchReducer from '@/redux/slices/matchSlice';
 import settingReducer from '@/redux/slices/settingSlice';
-
+import { configureStore } from '@reduxjs/toolkit';
 const store = configureStore({
 	reducer: {
 		match: matchReducer,
 		setting: settingReducer,
+		guide: guideReducer,
 	},
 });
 
