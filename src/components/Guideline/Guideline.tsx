282d4fd8beef1cceafbc36f9362d588e68cb8def
Tan IT <tandev151>
2025-01-08 23:11:48 +0700

    custom for tooltip

--- src/components/Guideline/Guideline.tsx
+++ src/components/Guideline/Guideline.tsx
@@ -1,11 +1,41 @@
-import Joyride, { Step } from 'react-joyride';
+import Joyride, { Step, TooltipRenderProps } from 'react-joyride';
 import { useGuide } from './hooks';
 import { GuideType } from './constants';
-
+import { Typography, Button, Box, Stack } from '@mui/material';
+import styles from './styles';
 interface Props {
 	steps: Step[];
 	type: GuideType;
 }
+
+const CustomTooltip = (props: TooltipRenderProps) => {
+	const { backProps, isLastStep, index, primaryProps, skipProps, step, tooltipProps, size } = props;
+	console.log({ primaryProps, step, tooltipProps });
+	return (
+		<Box sx={styles.tooltip} {...tooltipProps}>
+			{step.title && <Typography variant="h2">{step.title}</Typography>}
+			{step.content && <Typography sx={styles.content}>{step.content}</Typography>}
+			<Stack direction="row" justifyContent={isLastStep ? 'center' : 'space-between'}>
+				{!isLastStep && (
+					<Button variant="text" {...skipProps}>
+						SKIP
+					</Button>
+				)}
+				<Stack direction="row" gap={2}>
+					{index > 0 && (
+						<Button {...backProps} variant="outlined">
+							Quay lại
+						</Button>
+					)}
+					<Button
+						{...primaryProps}
+						variant="contained"
+					>{`${index + 1 < size ? 'Đi tiếp' : 'Kết thúc'} (${index + 1}/${size})`}</Button>
+				</Stack>
+			</Stack>
+		</Box>
+	);
+};
 
 function Guideline({ steps, type }: Props) {
 	const { guideType, handleJoyrideCallback } = useGuide();
@@ -25,6 +55,7 @@ function Guideline({ steps, type }: Props) {
 					zIndex: 10000,
 				},
 			}}
+			tooltipComponent={CustomTooltip}
 		/>
 	);
 }
--- src/components/Guideline/styles.ts
+++ src/components/Guideline/styles.ts
@@ -1,7 +1,15 @@
-const styles = {
-	content: {},
+import { SxProps } from '@mui/material';
 
-	skipBtn: {},
+const styles: { [key: string]: SxProps } = {
+	tooltip: {
+		backgroundColor: '#ffffff',
+		padding: 2,
+		borderRadius: 1,
+		width: '350px',
+	},
+	content: {
+		marginBottom: '24px',
+	},
 };
 
 export default styles;
